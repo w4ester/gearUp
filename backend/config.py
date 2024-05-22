e94aa2e19f344bd8374b3cc5455d6ebbeadd8dba
w4ester <w4planning@gmail.com>
2024-05-22 02:41:37 -0400

    Updated Dockerfile, add file

--- backend/config.py
+++ backend/config.py
@@ -71,13 +71,13 @@ for source in log_sources:
 
 log.setLevel(SRC_LOG_LEVELS["CONFIG"])
 
-WEBUI_NAME = os.environ.get("WEBUI_NAME", "Open WebUI")
-if WEBUI_NAME != "Open WebUI":
-    WEBUI_NAME += " (Open WebUI)"
+WEBUI_NAME = os.environ.get("WEBUI_NAME", "CareerUp")
+if WEBUI_NAME != "CareerUp":
+    WEBUI_NAME += " (CareerUp)"
 
-WEBUI_URL = os.environ.get("WEBUI_URL", "http://localhost:3000")
+WEBUI_URL = os.environ.get("CareerUp", "http://localhost:3000")
 
-WEBUI_FAVICON_URL = "https://openwebui.com/favicon.png"
+WEBUI_FAVICON_URL = "https://careerup.com/favicon.png"
 
 
 ####################################
--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-image_name="open-webui"
-container_name="open-webui"
+image_name="careerup"
+container_name="careerup"
 host_port=3000
 container_port=8080
 
